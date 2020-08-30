ca1ba498235d17cb81d314ed3ee7ac41602ac9ec
Kieran Hunt <hunkiera@amazon.com>
2020-08-30 08:45:11 +0200

    Add the Gre(y)t logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ export default function IndexPage() {
   return (
     <div>
       <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
+        <h1 className="text-5xl text-center">
+          Gre(y)t Water
         </h1>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'accent-1': '#333',
+        "sun-yellow": "",
+        "water-blue": "",
+        "plant-green": "",
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
